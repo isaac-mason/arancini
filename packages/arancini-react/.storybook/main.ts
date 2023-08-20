7470d2e0ff52383800855a9b3db1b31f8a77d99a
“Isaac <isaac@isaacmason.com>
2023-08-20 14:26:37 +1000

    chore: update storybook

--- packages/arancini-react/.storybook/main.ts
+++ packages/arancini-react/.storybook/main.ts
@@ -1,14 +1,15 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
   stories: ['./stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
   docs: {
@@ -20,3 +21,7 @@ const config: StorybookConfig = {
 }
 
 export default config
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
