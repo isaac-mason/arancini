a563ed629cac1c518373fcfc2fd70972b2cca2d0
“Isaac <isaac@isaacmason.com>
2023-12-24 14:15:51 +1000

    fix(react): tests

--- packages/arancini-react/tst/setup.ts
+++ packages/arancini-react/tst/setup.ts
@@ -1,8 +1,5 @@
-import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import matchers from '@testing-library/jest-dom/matchers'
-
-expect.extend(matchers)
+import { afterEach } from 'vitest'
 
 afterEach(() => {
   cleanup()
